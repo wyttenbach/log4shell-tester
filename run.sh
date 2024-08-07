a785e7db2ad90c655fc9e04fdb75decf89e21a5f
Dale Wyttenbach <dale.wyttenbach@gmail.com>
2024-08-07 12:20:56 -0500

    experimenting

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Example invocation listening on 127.0.0.1 for HTTP (default).
+#   This is recommended if running publicly so you can setup
+#   a proxy like nginx to handle SSL publicly.
+java -jar target/log4shell-jar-with-dependencies.jar \
+   --hostname wyttenbach.com \
+   --http-host 127.0.0.1 \
+   --http-port 8000 \
+   --ldap-host 0.0.0.0 \
+   --ldap-port 1389 \
+   --redis-url "redis://my-redis-url.something.com:6379"
